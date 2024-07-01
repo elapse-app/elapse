3034f077a1cbe1a7dab1e03a73f13e910ee4dfa0
Noah <noahsun0418@gmail.com>
2024-06-30 20:58:19 -0400

    bugfix: TeamStats tested and works
    - OPR and DPR calculations confirmed to be correct (compared with RoboScout)
    - Rounded OPR, DPR, and CCWM to 2 decimal places
    - Fixed many bugs with parsing and organizing data
    - Fixed OPR and DPR calculations
    - Added qualifier matches method to Schedule requests
    - Still requires some more error handling in case of incomplete data

--- elapse_app/lib/calculations/teamStats.dart
+++ elapse_app/lib/calculations/teamStats.dart
@@ -1,35 +1,35 @@
 import "dart:convert";
 
 import "package:elapse_app/classes/Tournament/game.dart";
-import "package:elapse_app/requests/teamLookup.dart";
-import "package:elapse_app/classes/Team/team.dart";
 
 import "package:ml_linalg/matrix.dart";
 import "package:http/http.dart" as http;
 import "dart:io";
 
+import "../requests/schedule.dart";
+
 class TeamStats {
-  int wins = 0;
-  int losses = 0;
-  int ties = 0;
+  num wins = 0;
+  num losses = 0;
+  num ties = 0;
 
-  int wp = 0;
-  int ap = 0;
-  int sp = 0;
+  num wp = 0;
+  num ap = 0;
+  num sp = 0;
 
-  double opr = 0;
-  double dpr = 0;
-  double ccwm = 0;
+  num opr = 0;
+  num dpr = 0;
+  num ccwm = 0;
   
-  int highScore = 0;
-  int avgScore = 0;
-  int ttlScore = 0;
+  num highScore = 0;
+  num avgScore = 0.0;
+  num ttlScore = 0;
 }
 
 class EventTeamStats {
-  Map<Team, TeamStats> stats = {};
+  Map<int, TeamStats> stats = {};
 
-  EventTeamStats();
+  EventTeamStats() {}
 
   static Future<EventTeamStats> calculate(int eventId, int divisionId) async {
     // Rankings data from robotevents
@@ -43,65 +43,86 @@ class EventTeamStats {
     if (rankings.statusCode != 200) {
       throw Exception("Failed to get rankings");
     }
-    final parsedRankings = jsonDecode(rankings.body) as List;
+    final parsedRankings = jsonDecode(rankings.body)["data"] as List;
 
-    // Matches data from robotevents
-    final matches = await http.get(
-      Uri.parse("https://www.robotevents.com/api/v2/events/$eventId/divions/$divisionId/matches?round%5B%5D=2"),
-      headers: {
-        HttpHeaders.authorizationHeader:
-          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiMTM4MmM0Y2Y4NTUxNGM1NzYwMDI5MDhlNWMzZWM2ODNkOGUwYzIwMWNiNjJjNThjYTQwZjc3MmI5YWYzOTE0ZGJiMDFhMGZmYWNjNWI3Y2UiLCJpYXQiOjE3MTg1NjU0MTUuODE1MzgzOSwibmJmIjoxNzE4NTY1NDE1LjgxNTM4OCwiZXhwIjoyNjY1MjUzODE1LjgwOTkwODksInN1YiI6IjEzMDkyNSIsInNjb3BlcyI6W119.OYTpCEjaa5Rzmdy2rHLpnGKBrV62Cg_Se5P5UoV_t1sf-NzgG3NMwcPFtOkh-ZVFKkfG7G4bsCl78KjiC8Qnmxd7TXRad_XoTaSVDFqMZlYMLlbSjjNw2lqo_O3UP-SMi-Gei6f9qc3Zs09uqEAnb1v7f2-qxu4YGAcsOEjHtYTURHJY6ZeRCrewZpI_6fGrQwdNqXSc_kNvO6ygIGOrK2pMJGm-WCiWzfKAdLg0L8gWlemLNeJumoe38XbYMnJ2upBzc-SGdsPf33_yvWmR15wBqpqqoO0b57KoLpzzppFBBQgJlqNjUOUs8MFlMVlbK1DVx8PdfvU1R--nVgQm8gWlV0IvaIcDa9Yqrz0IblrCco7T5adIM9tPSuRBdetD6dhqpGZEzUaqjO1f0AVQc3Vs7HsGsLGG2_D11Wwpl8texMTJpHCI6tV4fc5msi7kRdwBJ7PgsYkkJKVQV2Q1-Whk5xHvFZyiTYKkR_eHrNshCgL-n5FFqttbkZ_xP3q9qMsl93UVWCwE5zi-SnyJn2cmXKWAfGRsbSvge-UWlEduz_yGdfhSKjjhLfnkjZDmZANlY0_ylEnAUMwAy7oEFyrxf92PFsNzzXGfmVtowbhT1WOpltpZOeOFqS-lUm_A1UXE__OiKc5JeS8zjf2wxlZ1x9TKaFn5yHBJUwht8KA",
-      },
-    );
-    if (matches.statusCode != 200) {
-      throw Exception("Failed to get matches");
-    }
-    final parsedMatches = jsonDecode(matches.body) as List;
 
+    List<Game>? matches = await getQualifierMatches(eventId, divisionId);
 
     EventTeamStats teamStats = EventTeamStats();
-    Map<Team, TeamStats> stats = teamStats.stats;
+    Map<int, TeamStats> stats = teamStats.stats;
 
     // Process Robotevents stats
+    stats.addAll(Map<int, TeamStats>.fromEntries(parsedRankings.map((v) => MapEntry(v["team"]["id"], TeamStats()))));
     for (final t in parsedRankings) {
-      Team team = await fetchTeam(t["team"]["id"]);
-      
-      stats.putIfAbsent(team, () => TeamStats());
+      int teamId = t["team"]["id"];
 
-      stats[team]?.wins = t["wins"];
-      stats[team]?.losses = t["losses"];
-      stats[team]?.ties = t["ties"];
+      stats[teamId]?.wins = t["wins"] ?? 0;
+      stats[teamId]?.losses = t["losses"] ?? 0;
+      stats[teamId]?.ties = t["ties"] ?? 0;
 
-      stats[team]?.wp = t["wp"];
-      stats[team]?.ap = t["ap"];
-      stats[team]?.sp = t["sp"];
+      stats[teamId]?.wp = t["wp"];
+      stats[teamId]?.ap = t["ap"];
+      stats[teamId]?.sp = t["sp"];
 
-      stats[team]?.highScore = t["high_score"];
-      stats[team]?.avgScore = t["average_score"];
-      stats[team]?.ttlScore = t["total_score"];
+      stats[teamId]?.highScore = t["high_score"] ?? 0;
+      stats[teamId]?.avgScore = (t["average_points"] ?? 0.0).toDouble();
+      stats[teamId]?.ttlScore = t["total_points"] ?? 0;
+    }
+
+    int teamsLastPage = jsonDecode(rankings.body)["meta"]["last_page"];
+    for (int pg = 2; pg <= teamsLastPage; pg++) {
+      final pgResponse = await http.get(
+        Uri.parse("https://www.robotevents.com/api/v2/events/$eventId/divisions/$divisionId/rankings?page=$pg"),
+        headers: {
+          HttpHeaders.authorizationHeader:
+          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiMTM4MmM0Y2Y4NTUxNGM1NzYwMDI5MDhlNWMzZWM2ODNkOGUwYzIwMWNiNjJjNThjYTQwZjc3MmI5YWYzOTE0ZGJiMDFhMGZmYWNjNWI3Y2UiLCJpYXQiOjE3MTg1NjU0MTUuODE1MzgzOSwibmJmIjoxNzE4NTY1NDE1LjgxNTM4OCwiZXhwIjoyNjY1MjUzODE1LjgwOTkwODksInN1YiI6IjEzMDkyNSIsInNjb3BlcyI6W119.OYTpCEjaa5Rzmdy2rHLpnGKBrV62Cg_Se5P5UoV_t1sf-NzgG3NMwcPFtOkh-ZVFKkfG7G4bsCl78KjiC8Qnmxd7TXRad_XoTaSVDFqMZlYMLlbSjjNw2lqo_O3UP-SMi-Gei6f9qc3Zs09uqEAnb1v7f2-qxu4YGAcsOEjHtYTURHJY6ZeRCrewZpI_6fGrQwdNqXSc_kNvO6ygIGOrK2pMJGm-WCiWzfKAdLg0L8gWlemLNeJumoe38XbYMnJ2upBzc-SGdsPf33_yvWmR15wBqpqqoO0b57KoLpzzppFBBQgJlqNjUOUs8MFlMVlbK1DVx8PdfvU1R--nVgQm8gWlV0IvaIcDa9Yqrz0IblrCco7T5adIM9tPSuRBdetD6dhqpGZEzUaqjO1f0AVQc3Vs7HsGsLGG2_D11Wwpl8texMTJpHCI6tV4fc5msi7kRdwBJ7PgsYkkJKVQV2Q1-Whk5xHvFZyiTYKkR_eHrNshCgL-n5FFqttbkZ_xP3q9qMsl93UVWCwE5zi-SnyJn2cmXKWAfGRsbSvge-UWlEduz_yGdfhSKjjhLfnkjZDmZANlY0_ylEnAUMwAy7oEFyrxf92PFsNzzXGfmVtowbhT1WOpltpZOeOFqS-lUm_A1UXE__OiKc5JeS8zjf2wxlZ1x9TKaFn5yHBJUwht8KA",
+        },
+      );
+      if (pgResponse.statusCode != 200) {
+        throw Exception("Failed to get next page");
+      }
+      final parsedPg = jsonDecode(pgResponse.body)["data"] as List;
+
+      stats.addAll(Map<int, TeamStats>.fromEntries(parsedPg.map((v) => MapEntry(v["team"]["id"], TeamStats()))));
+      for (final t in parsedPg) {
+        int teamId = t["team"]["id"];
+
+        stats[teamId]?.wins = t["wins"] ?? 0;
+        stats[teamId]?.losses = t["losses"] ?? 0;
+        stats[teamId]?.ties = t["ties"] ?? 0;
+
+        stats[teamId]?.wp = t["wp"];
+        stats[teamId]?.ap = t["ap"];
+        stats[teamId]?.sp = t["sp"];
+
+        stats[teamId]?.highScore = t["high_score"] ?? 0;
+        stats[teamId]?.avgScore = (t["average_points"] ?? 0.0).toDouble();
+        stats[teamId]?.ttlScore = t["total_points"] ?? 0;
+      }
     }
 
     // Process matches
     List<double> redScores = [];
     List<double> blueScores = [];
-    List<Map<Team, double>> redMatchTeams = List.generate(parsedMatches.length, (_) {
+
+    List<Map<int, double>> redMatchTeams = List.generate(matches!.length, (_) {
       return Map.fromIterables(stats.keys, List<double>.filled(stats.keys.length, 0.0));
     });
-    List<Map<Team, double>> blueMatchTeams = List.generate(parsedMatches.length, (_) {
+    List<Map<int, double>> blueMatchTeams = List.generate(matches.length, (_) {
       return Map.fromIterables(stats.keys, List<double>.filled(stats.keys.length, 0.0));
     });
 
     // Determine which teams played in each match and each match's scores
-    for (int i = 0; i < parsedMatches.length; i++) {
-      Game match = parsedMatches[i];
+    for (int i = 0; i < matches.length; i++) {
+      Game match = matches[i];
 
       redScores.add((match.redScore ?? 0).toDouble());
       blueScores.add((match.blueScore ?? 0).toDouble());
 
-      redMatchTeams[i][await fetchTeam(match.redAllianceID![0])] = 1.0;
-      redMatchTeams[i][await fetchTeam(match.redAllianceID![1])] = 1.0;
-      blueMatchTeams[i][await fetchTeam(match.blueAllianceID![0])] = 1.0;
-      blueMatchTeams[i][await fetchTeam(match.blueAllianceID![1])] = 1.0;
+      redMatchTeams[i][match.redAllianceID![0]] = 1.0;
+      redMatchTeams[i][match.redAllianceID![1]] = 1.0;
+      blueMatchTeams[i][match.blueAllianceID![0]] = 1.0;
+      blueMatchTeams[i][match.blueAllianceID![1]] = 1.0;
     }
 
     // OPR and DPR calculations
@@ -109,8 +130,8 @@ class EventTeamStats {
     List<double> oppScores = blueScores + redScores;
     List<List<double>> matchTeams = redMatchTeams.map((map) => map.values.toList()).toList() + blueMatchTeams.map((map) => map.values.toList()).toList();
     
-    Matrix mScores = Matrix.column(scores + oppScores);
-    Matrix mOppScores = Matrix.column(oppScores + scores);
+    Matrix mScores = Matrix.column(scores);
+    Matrix mOppScores = Matrix.column(oppScores);
     Matrix mMatches = Matrix.fromList(matchTeams);
     Matrix mMatchesT = mMatches.transpose();
 
@@ -121,9 +142,9 @@ class EventTeamStats {
 
     int i = 0;
     for (var stat in stats.values) {
-      stat.opr = opr[i];
-      stat.dpr = dpr[i];
-      stat.ccwm = stat.opr - stat.dpr;
+      stat.opr = double.parse(opr[i].toStringAsFixed(2));
+      stat.dpr = double.parse(dpr[i].toStringAsFixed(2));
+      stat.ccwm = double.parse((stat.opr - stat.dpr).toStringAsFixed(2));
       i++;
     }
 
