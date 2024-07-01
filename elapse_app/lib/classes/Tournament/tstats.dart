801f2ba1ca61af4d53c059753f1ea519092e713c
Noah <noahsun0418@gmail.com>
2024-07-01 13:28:57 -0400

    refactor: Data requests now use Future.wait()

--- elapse_app/lib/classes/Tournament/tstats.dart
+++ elapse_app/lib/classes/Tournament/tstats.dart
@@ -35,20 +35,27 @@ Future<Map<int, TeamStats>> calcEventStats(int eventId, int divisionId) async {
   Map<int, TeamStats> stats = {};
 
   // Get qualifier matches
-  List<Game>? matches = (await getTournamentSchedule(eventId, divisionId))?.where((f) => f.roundNum == 2).toList();
+  List<Game> matches = [];
+  var rankings;
+  List parsedRankings = [];
 
-  // Rankings data from robotevents
-  final rankings = await http.get(
+  List<Future<void>> requestFutures = [];
+  requestFutures.add(getTournamentSchedule(eventId, divisionId)!.then(
+          (m) => matches = m.where((e) => e.roundNum == 2).toList()));
+  requestFutures.add(http.get(
     Uri.parse("https://www.robotevents.com/api/v2/events/$eventId/divisions/$divisionId/rankings"),
     headers: {
       HttpHeaders.authorizationHeader:
       "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiMTM4MmM0Y2Y4NTUxNGM1NzYwMDI5MDhlNWMzZWM2ODNkOGUwYzIwMWNiNjJjNThjYTQwZjc3MmI5YWYzOTE0ZGJiMDFhMGZmYWNjNWI3Y2UiLCJpYXQiOjE3MTg1NjU0MTUuODE1MzgzOSwibmJmIjoxNzE4NTY1NDE1LjgxNTM4OCwiZXhwIjoyNjY1MjUzODE1LjgwOTkwODksInN1YiI6IjEzMDkyNSIsInNjb3BlcyI6W119.OYTpCEjaa5Rzmdy2rHLpnGKBrV62Cg_Se5P5UoV_t1sf-NzgG3NMwcPFtOkh-ZVFKkfG7G4bsCl78KjiC8Qnmxd7TXRad_XoTaSVDFqMZlYMLlbSjjNw2lqo_O3UP-SMi-Gei6f9qc3Zs09uqEAnb1v7f2-qxu4YGAcsOEjHtYTURHJY6ZeRCrewZpI_6fGrQwdNqXSc_kNvO6ygIGOrK2pMJGm-WCiWzfKAdLg0L8gWlemLNeJumoe38XbYMnJ2upBzc-SGdsPf33_yvWmR15wBqpqqoO0b57KoLpzzppFBBQgJlqNjUOUs8MFlMVlbK1DVx8PdfvU1R--nVgQm8gWlV0IvaIcDa9Yqrz0IblrCco7T5adIM9tPSuRBdetD6dhqpGZEzUaqjO1f0AVQc3Vs7HsGsLGG2_D11Wwpl8texMTJpHCI6tV4fc5msi7kRdwBJ7PgsYkkJKVQV2Q1-Whk5xHvFZyiTYKkR_eHrNshCgL-n5FFqttbkZ_xP3q9qMsl93UVWCwE5zi-SnyJn2cmXKWAfGRsbSvge-UWlEduz_yGdfhSKjjhLfnkjZDmZANlY0_ylEnAUMwAy7oEFyrxf92PFsNzzXGfmVtowbhT1WOpltpZOeOFqS-lUm_A1UXE__OiKc5JeS8zjf2wxlZ1x9TKaFn5yHBJUwht8KA",
     },
-  );
-  if (rankings.statusCode != 200) {
-    throw Exception("Failed to get rankings");
-  }
-  final parsedRankings = jsonDecode(rankings.body)["data"] as List;
+  ).then((rankingsResponse) {
+    rankings = rankingsResponse;
+    if (rankingsResponse.statusCode != 200) {
+      throw Exception("Failed to get rankings");
+    }
+    parsedRankings = jsonDecode(rankingsResponse.body)["data"] as List;
+  }));
+  await Future.wait(requestFutures);
 
   stats.addAll(Map<int, TeamStats>.fromEntries(parsedRankings.map((v) => MapEntry(v["team"]["id"], TeamStats()))));
   for (final t in parsedRankings) {
@@ -87,6 +94,8 @@ Future<Map<int, TeamStats>> calcEventStats(int eventId, int divisionId) async {
         for (final t in parsedPg) {
           int teamId = t["team"]["id"];
 
+          stats[teamId]?.rank = t["rank"] ?? 0;
+
           stats[teamId]?.wins = t["wins"] ?? 0;
           stats[teamId]?.losses = t["losses"] ?? 0;
           stats[teamId]?.ties = t["ties"] ?? 0;
@@ -108,7 +117,7 @@ Future<Map<int, TeamStats>> calcEventStats(int eventId, int divisionId) async {
   List<double> redScores = [];
   List<double> blueScores = [];
 
-  List<Map<int, double>> redMatchTeams = List.generate(matches!.length, (_) {
+  List<Map<int, double>> redMatchTeams = List.generate(matches.length, (_) {
     return Map.fromIterables(stats.keys, List<double>.filled(stats.keys.length, 0));
   });
   List<Map<int, double>> blueMatchTeams = List.generate(matches.length, (_) {
