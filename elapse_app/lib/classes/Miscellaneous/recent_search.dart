a64f01fe46757eec22508098b0f9bbf0c39f89be
arrana16 <69128309+arrana16@users.noreply.github.com>
2024-07-24 03:11:09 +0300

    feat: added basic layout for explore page

---
+++ elapse_app/lib/classes/Miscellaneous/recent_search.dart
@@ -0,0 +1,11 @@
+class RecentSearch {
+  final String searchTerm;
+  final int? teamID;
+  final int? tournamentID;
+
+  RecentSearch({
+    required this.searchTerm,
+    this.teamID,
+    this.tournamentID,
+  });
+}
