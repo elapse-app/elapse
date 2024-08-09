7555a674f22e4fe31f22c929cc2898913703ee50
Noah <noahsun0418@gmail.com>
2024-08-09 15:23:31 -0400

    Feat. Continued work on Worlds Rankings page

--- elapse_app/lib/screens/explore/worldRankings.dart
+++ elapse_app/lib/screens/explore/worldRankings.dart
@@ -1,9 +1,8 @@
-import 'package:elapse_app/screens/explore/worldRankings/worldSkills.dart';
-import 'package:elapse_app/screens/explore/worldRankings/worldTrueSkill.dart';
+import 'package:elapse_app/screens/explore/worldRankings/world_skills.dart';
+import 'package:elapse_app/screens/explore/worldRankings/world_true_skill.dart';
+import 'package:elapse_app/classes/Team/vdaStats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import '../widgets/rounded_top.dart';
 
 class WorldRankingsScreen extends StatefulWidget {
   int initState = 0;
@@ -25,87 +24,39 @@ class _WorldRankingsState extends State<WorldRankingsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> pages = [
-      WorldSkills(),
-      WorldTrueSkill(),
-    ];
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar.large(
-            automaticallyImplyLeading: false,
-            expandedHeight: 125,
-            centerTitle: false,
-            flexibleSpace: FlexibleSpaceBar(
-              expandedTitleScale: 1,
-              collapseMode: CollapseMode.parallax,
-              title: Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      padding: const EdgeInsets.only(top: 10),
-                      constraints: BoxConstraints(),
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    Text(
-                      titles[selectedIndex],
-                      style: const TextStyle(
-                        fontSize: 30, fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const Spacer(),
-                  ]
-                )
-              ),
-              centerTitle: false,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(200),
+          child: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "World Rankings",
+                  style: const TextStyle(
+                      fontSize: 30, fontWeight: FontWeight.w600),
+                ),
+                const Spacer(),
+              ]
             ),
-            backgroundColor: Theme.of(context).colorScheme.primary,
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "Skills"),
+                Tab(text: "TrueSkill"),
+              ],
+            ),
           ),
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: SliverHeaderDelegate(
-              minHeight: 70.0,
-              maxHeight: 70.0,
-              child: Hero(
-                tag: "top",
-                child: Stack(
-                  children: [
-                    Container(
-                      height: 300,
-                      color: Theme.of(context).colorScheme.primary
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.surface,
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(25),
-                          topRight: Radius.circular(25),
-                        )
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 13),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-
-                          ]
-                        )
-                      )
-                    )
-                  ]
-                )
-              )
-            )
-          )
-        ]
-      )
+        ),
+        body: TabBarView(
+          children: [
+            WorldSkillsPage(rankings: getTrueSkillData()),
+            WorldTrueSkill(),
+          ],
+        ),
+      ),
     );
   }
 }
