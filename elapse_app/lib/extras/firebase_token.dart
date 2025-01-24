e7fa1a1998f7d9bfb827c56ea1d67df9084a7c0a
adencoco <adengnorman@gmail.com>
2025-01-23 20:09:08 -0800

    tokens

---
+++ elapse_app/lib/extras/firebase_token.dart
@@ -0,0 +1,24 @@
+class firebaseTokens {
+  static const messagingSenderID = '1085802258375';
+
+  // web
+  static const web_api_key = 'AIzaSyB1X33mMM1BDnFetoI17fzxU8hhSsX7t8k';
+  static const web_app_id = '1:1085802258375:web:c343ecbb473702310c03e2';
+  // android
+  static const android_api_key = 'AIzaSyDoRS0lYO4_ljPK5G9KJcYdo-33FRUNjec';
+  static const android_app_id = '1:1085802258375:android:a445f127ec8ae9650c03e2';
+
+  // ios
+  static const ios_api_key = 'AIzaSyA9NIyftybAfImb8wteA0s1UV0t_gqZgB4';
+  static const ios_app_id = '1:1085802258375:ios:ca34fc646b6a50bf0c03e2';
+
+  // macos
+
+  static const mac_api_key = 'AIzaSyA9NIyftybAfImb8wteA0s1UV0t_gqZgB4';
+  static const mac_app_id = '1:1085802258375:ios:ad62a372ff6464ea0c03e2';
+
+  // windows
+
+  static const windows_api_key =  'AIzaSyB1X33mMM1BDnFetoI17fzxU8hhSsX7t8k';
+  static const windows_app_id = '1:1085802258375:web:12a6e56feecf5b6e0c03e2';
+}
