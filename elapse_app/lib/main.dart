fe8f21361687c0e0d7fba6c3456136afb26b2a56
adencoco <adengnorman@gmail.com>
2025-02-22 21:02:09 -0800

    Update main.dart

--- elapse_app/lib/main.dart
+++ elapse_app/lib/main.dart
@@ -70,7 +70,18 @@ void main() async {
   ErrorWidget.builder = (FlutterErrorDetails details) {
     return ErrorPage();
   };
+
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  print('Got a message whilst in the foreground!');
+  print('Message data: ${message.data}');
+
+  if (message.notification != null) {
+    print('Message also contained a notification: ${message.notification}');
+  }
+  });
+
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
